cb091190ca7574eefeb50ee9684b1cfe8d513ca1
Joey Phan <josephkphan@gmail.com>
2018-01-22 00:54:45 -0800

    adding persistent volumes, and ingress stuff

--- Docker/app.py
+++ Docker/app.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from redis import Redis
 import os
+from random import *
 
 app = Flask(__name__)
 redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'),port=6379)
+random_id = randint(1, 1000)
 
 @app.route('/')
 def display_counter():
     redis.incr('hits')
     return 'This application has been hit %s times.\n' % redis.get('hits')
+
+@app.route('/echo')
+def echo():
+    return 'My Random ID is %s.\n' % str(random_id)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
